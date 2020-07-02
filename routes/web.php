8b0940752295c519c26280c499698a86d7c6d0d0
Rofiq R. Murod <rofiqmurod@gmail.com>
2020-07-02 20:29:27 +0700

    Copy Layout Templating Blade

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,6 +13,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function() {
+	return view('isi.index');
+});
+
+Route::get('/data-tables', function() {
+	return view('isi.data');
 });
